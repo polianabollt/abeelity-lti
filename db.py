b2024a8b5e79217e14d86e4901e7b4c26d18ba76
Poliana Boll Thiele <polianabollt@gmail.com>
2025-07-14 15:32:18 -0300

    novo db

--- db.py
+++ db.py
@@ -1,15 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from sqlmodel import SQLModel, create_engine, Session
+import os
 
-DATABASE_URL = "postgresql://abeelity_user:4HfQeVkqAjm%3DXK%5F@localhost:5432/abeelity_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não encontrado no .env")
+
 engine = create_engine(DATABASE_URL, echo=True)
-
-
-def get_session():
-    return Session(engine)
-
-def create_db_and_tables():
-    from models import LMSPlatform
-    SQLModel.metadata.create_all(engine)
-
-def create_db():
-    SQLModel.metadata.create_all(engine)
