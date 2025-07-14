56a6952a441c83b1dd2d3bddfeec402edb32e4bc
Poliana Boll Thiele <polianabollt@gmail.com>
2025-07-14 12:49:53 -0300

    arrumand oget

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from routers import lti, usage
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Abeelity LTI está rodando!"}
+
 app.include_router(lti.router, prefix="/lti")
 #app.include_router(usage.router, prefix="/admin")
