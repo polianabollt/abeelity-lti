2da3b255a8820d16d99a9bcdc719240d845f27ee
Poliana Boll Thiele <polianabollt@gmail.com>
2025-07-14 16:56:30 -0300

    init

--- init_db.py
+++ init_db.py
@@ -1,5 +1,5 @@
-# init_db.py
-from db import create_db_and_tables
+from db import create_db_and_tables, engine
 
 if __name__ == "__main__":
+    print("🧪 DATABASE_URL usado por init_db.py:", engine.url)
     create_db_and_tables()
